b136afb9e08872fef8b7942092a2c227238cd23d
KannadaCinemaStore <kannadacinemastore@gmail.com>
2026-03-23 21:47:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -176,9 +176,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://evident-jillana-kannadaflixmoviez-bb952645.koyeb.app/".format(FQDN)
+    URL = "https://relevant-vinita-kannadaflixmoviez-449f2c49.koyeb.app/".format(FQDN)
 else:
-    URL = "http://evident-jillana-kannadaflixmoviez-bb952645.koyeb.app/".format(FQDN)
+    URL = "https://relevant-vinita-kannadaflixmoviez-449f2c49.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration
